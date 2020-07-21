e538565642a3a95426a62587026c2c97a316531c
Hossein <hossein.dehghanipour1998@gmail.com>
2020-07-21 19:59:32 +0430

    before implementing Amir-erfan Implementationa

--- online-class/src/components/EditProfileForm.vue
+++ online-class/src/components/EditProfileForm.vue
@@ -1,59 +1,54 @@
 <template>
 
 
-<div class="SignInAlignment">
-<main class="black-80 full">
-  <form class="center ">
-    <fieldset id="sign_up" style="padding:10px;flex:1;" class="ba b--transparent ph0 mh0">
-      <h3 style="color:white"> Edit Profile </h3>
+<div id="kk" class="SignInAlignment">
+    <form >
+      <fieldset id="sign_up" style="padding:10px;">
+        <h3 style="color:white"> Edit Profile </h3>
+        <div class="row">
+          <div class="column">
+              <input  class="inputStyle pa2 inputStyle" type="text"  placeholder="First Name ">
+          </div>
+          <div class="column">
+              <input class="inputStyle  pa2 inputStyle" type="text"  placeholder="Last Name ">
+          </div>
+        </div>
 
-      <div class="row">
-        <div class="column">
-            <input  class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="text"  placeholder="First Name ">
+        <div class="row pa2" style="width:100% ">
+            <input class="inputStyle  pa2 inputStyle" type="email" name="email-address" placeholder="Email Address" id="email-address">
         </div>
-        <div class="column">
-            <input class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="text"  placeholder="Last Name ">
+
+        <div class="row pa2" style="width:100% ">
+          <input class="inputStyle  pa2  inputStyle" type="password" name="password"  id="password" placeholder="Password">
         </div>
-      </div>
 
       <div class="row pa2" style="width:100% ">
-          <input class="inputStyle  pa2 input-reset ba b--transparent bg-transparent   inputStyle" type="email" name="email-address" placeholder="Email Address" id="email-address">
-      </div>
-
-      <div class="row pa2" style="width:100% ">
-        <input class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="password" name="password"  id="password" placeholder="Password">
-      </div>
-
-     <div class="row pa2" style="width:100% ">
-        <input class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="password" name="password"  id="ConfirmPassword" placeholder="Confirm Password">
-      </div>
-
-
-      <div class="row">
-        <div class="column3">
-            <input  class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="text"  placeholder="Postal Code ">
-        </div>
-        <div class="column3">
-            <input class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="text"  placeholder="Country">
+          <input class="inputStyle  pa2 inputStyle" type="password" name="password"  id="ConfirmPassword" placeholder="Confirm Password">
         </div>
 
-        <div class="column3">
-            <input class="inputStyle  pa2 input-reset ba b--transparent bg-transparent    inputStyle" type="text"  placeholder="City">
+
+        <div class="row">
+          <div class="column3">
+              <input  class="inputStyle  pa2 inputStyle" type="text"  placeholder="Postal Code ">
+          </div>
+          <div class="column3">
+              <input class="inputStyle  pa2 inputStyle" type="text"  placeholder="Country">
+          </div>
+
+          <div class="column3">
+              <input class="inputStyle  pa2 inputStyle" type="text"  placeholder="City">
+          </div>
         </div>
-      </div>
 
-      <div class="row pa2" style="width:100%">
-        <textarea class="inputStyle pa2" name="message" rows="6" style="width:100%;" placeholder="Describe yourself here..."></textarea>
-      </div>
-      <div class="row pa2" >
-        <input class=" buttonStyle b ph3 pv2    bg-transparent grow pointer  f6 dib" style ="width:auto;height:auto; float:right;" type="submit" value="Save">
-        <input class=" buttonStyle b ph3 pv2    bg-transparent grow pointer  f6 dib" style ="width:auto;height:auto; float:left;" type="submit" value="Reset">
-      </div>
-
-    </fieldset>
-
-  </form>
-</main>
+        <div class="row pa2" style="width:100%">
+          <textarea class="inputStyle pa2" name="message" rows="6" style="width:100%;" placeholder="Describe yourself here..."></textarea>
+        </div>
+        <div class="row pa2" >
+          <input class=" buttonStyle b ph3 pv2    bg-transparent grow pointer  f6 dib" style ="width:auto;height:auto; float:right;" type="submit" value="Save">
+          <input class=" buttonStyle b ph3 pv2    bg-transparent grow pointer  f6 dib" style ="width:auto;height:auto; float:left;" type="submit" value="Reset">
+        </div>
+      </fieldset>
+    </form>
 </div>
 </template>
 
@@ -61,40 +56,27 @@
  @import "../CSSFiles/Forms.css";
 .SignInAlignment {
 	font-family: "Comic Sans MS", cursive, sans-serif ;
-	width: 80%;
-	height: 90%;
-  flex:1;
-  	position: absolute;
-  	left: 50%;
-  	top: 50%;
-  	transform: translate(-50%, -50%);
-    box-shadow :0 4px 8px 0 rgb(0, 0, 0), 0 6px 20px 0 rgba(0, 0, 0, 1);
-    box-shadow :  1 8px 16px 0 rgba(0,0,0,1);
-    opacity: 1 !important;
+	width: 100px;
+  height: 100px;
+  box-shadow :  rgba(0,0,0,1);
+  opacity: 1 !important;
 }
 
-.paddingRight{
-  margin-right: 60px !important;
-}
+
 .inputStyle {
     color: blanchedalmond !important;
     border-radius: 12px;
-	background-color: #01142F ;
+	  background-color: #01142F ;
     background-color: #01142F!important;
-	transition-duration: 0.4s;
+	  transition-duration: 0.4s;
     border: 2px solid #008CBA;
     width: 100%;
-    font-size: 1.5rem;
+    font-size: 1rem;
 }
 
 .inputStyle:hover{
 
   border-color:purple;
-}
-
-.inputStyle:click{
-	border-block-color: pink;
-
 }
 
 
@@ -124,25 +106,10 @@
   clear: both;
 }
 
-.full{
-    widows: 100%;
-    height: 100%;
-}
-
-
-.card {
-  /* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-}
-
-/* On mouse-over, add a deeper shadow */
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-/* Add some padding inside the card container */
-.container {
-  padding: 2px 16px;
+#kk {
+  display: flex;
+  flex:1;
+  height: 100%;
+  width: 100%;
 }
 </style>
--- online-class/src/components/Home/Home.vue
+++ online-class/src/components/Home/Home.vue
@@ -1,37 +1,39 @@
 <template>
 
-    <div style="max-width:100%;height:100%;">
+    <div id = "father" style="">
 
-        <table style="width:100%;height:100%;">
-            <tbody>
+        <div  style="width:100%;height:100%;">
 
-                <tr style="height:20%; width:100%;padding:2px;">
-                    <td colspan="2">
+
+                <div id="upperRow"  >
+                    <div id="header">
                         <Header></Header>
-                    </td>
-                </tr>
+                    </div>
+                </div>
 
-                <tr style="height:80%; width:100%; background-color:pink; ">
-                    <td style="width:10% ; height:100%; background-color:pink;">
+                <div id="lowerRow"  >
+
+                    <div id="leftbox">
                         <SideBar></SideBar>
-                    </td>
-                    <td style="width:90% ; height:100%;background-color:red;">
-                        <div style="backgroundcolor:purple;"></div>
-                    </td>
-                </tr>
+                    </div>
 
-            </tbody>
-        </table>
+                    <div  id="rightbox">
+                    </div>
+
+                </div>
+        </div>
     </div>
 </template>
 
 <script>
 import SideBar from '../VerticalList/SideBar.vue' ;
 import Header from '../Header/Header.vue' ;
+import EditProfileForm from '../EditProfileForm.vue'
 export default {
     components: {
         SideBar,
-        Header
+        Header,
+        EditProfileForm
     }
 
 }
@@ -39,4 +41,66 @@ export default {
 
 <style scoped>
 
+.column {
+    float:left;
+    width: 50%;
+    padding: 10px;
+
+  }
+  /* Clear floats after the columns */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  .full{
+      widows: 100%;
+      height: 100%;
+  }
+
+
+#leftbox {
+
+    float:left;
+    width:20%;
+    height:100%;
+}
+#father{
+    display: flex;
+    flex:1;
+    height:100%;
+    max-width:100%;
+    margin:10px;
+}
+
+
+#header{
+height:100%; width:100%;
+}
+
+#rightbox{
+    display: flex;
+    flex:1;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    float:right;
+    width:75%;
+    height:100%;
+    background-color:purple;
+
+}
+#upperRow{
+height:20%;
+width:100%;
+padding:2px;
+}
+
+#lowerRow {
+    display: flex;
+    flex:1;
+    height: 100%;
+    width:100%;
+    background-color:pink;
+}
 </style>
