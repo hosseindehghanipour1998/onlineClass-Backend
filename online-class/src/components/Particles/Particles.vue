a160e80c7f0a08e1e742701a011f4ea4b998d5fd
Hossein <hossein.dehghanipour1998@gmail.com>
2020-07-24 04:20:53 +0430

    Relocated Particles

---
+++ online-class/src/components/Particles/Particles.vue
@@ -0,0 +1,20 @@
+<template>
+        <vue-particles
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="100"
+        :moveSpeed="3"
+        :clickEffect="false"
+      >
+      </vue-particles>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
