85c9addc90b5c8cef138cf148ba5b8b32e861eef
Hossein <hossein.dehghanipour1998@gmail.com>
2020-07-23 05:27:57 +0430

    FRixed some bugs

--- online-class/src/components/VerticalList/SideBar.vue
+++ online-class/src/components/VerticalList/SideBar.vue
@@ -23,7 +23,7 @@ export default {
                 {Text :"List Quizes", Emit : "LQ"},
                 {Text :"Create Class", Emit : "CC"},
                 {Text :"List Classes", Emit : "LC"},
-                {Text :"Create User", Emit : "CU"},
+                {Text :"Add Students", Emit : "AS"},
                 {Text :"List Users",Emit : "LU"}
             ],
         }
