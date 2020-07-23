4aa6fd6df552d08eb4fad35deeabe41c501bc7c7
Hossein <hossein.dehghanipour1998@gmail.com>
2020-07-24 04:20:30 +0430

    Added CreateQuizLayout

---
+++ online-class/src/components/CreateQuizLayout/CreateQuizLayout.vue
@@ -0,0 +1,192 @@
+<template>
+<div class="father">
+
+<div class="LittleBrother">
+    <input class="inputStyle" style="width:60%; min-height:3%;" placeholder="Quiz Name" />
+</div>
+<hr class="vl"/>
+
+ <div class="mother" v-for="(input,k) in inputs" :key="k">
+     <div class="oldBrother">
+        <span class="leftSpan">
+            <CreateQuizLayoutQuestion :QuestionNo="k"> </CreateQuizLayoutQuestion>
+        </span>
+        <span class="rightSpan">
+            <button class="buttonStyle" @click="remove(k)" v-show="k || ( !k && inputs.length > 1)">Remove  </button>
+            <button class="buttonStyle" @click="add(k)" v-show="k == inputs.length-1"> Add</button>
+        </span>
+    </div>
+
+</div>
+<div class="LittleBrother">
+    <button class="glow-on-hover" style="width:60%;"> Create </button>
+</div>
+
+</div>
+</template>
+
+<script>
+import CreateQuizLayoutQuestion from "./CreateQuizLayoutQuestion.vue";
+export default {
+    components:{
+        CreateQuizLayoutQuestion,
+    },
+    data(){
+        return{
+             inputs: [
+                {
+                    name: ''
+                }
+            ]
+        }
+
+    },
+    methods: {
+        add(index) {
+            this.inputs.push({ name: '' });
+        },
+        remove(index) {
+            this.inputs.splice(index, 1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.vl {
+  border: 2px solid white;
+  width: 100%;
+}
+.buttonStyle{
+  border-radius: 12px;
+  background-color: powderblue!important;
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  align-content: center;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border: 2px solid #008CBA;
+  margin:4px;
+}
+
+.buttonStyle:hover {
+  background-image: linear-gradient(89deg,#3CBA92,#0BA360);
+  color: black !important;
+}
+
+.father{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding:10px;
+    width: 100%;
+    height: 100%;
+}
+.mother{
+    width: 80%;
+    height: 100%;
+    margin-bottom: 5px;
+    margin-top: 5px;
+    padding: 2px;
+    width: 60%;
+    height: auto;
+    border:2px solid white;
+    border-radius: 5px;
+    background-color:gray ;
+}
+
+.mother:hover{
+    background-color: gold;
+}
+
+div{
+    width:100%;
+    height:100% px;
+}
+
+.leftSpan{
+    width: 90%;
+}
+
+.rightSpan{
+    width: 10%;
+    margin-left : 2px;
+}
+.inputStyle {
+    color: blanchedalmond !important;
+    border-radius: 12px;
+	background-color: #01142F ;
+    background-color: #01142F!important;
+	transition-duration: 0.4s;
+    border: 2px solid #008CBA;
+    width: 100%;
+    font-size: 1.5rem;
+}
+
+
+.glow-on-hover {
+    width: 220px;
+    height: 50px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background-color: powderblue !important;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+    opacity: 1;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+</style>
---
+++ online-class/src/components/CreateQuizLayout/CreateQuizLayoutQuestion.vue
@@ -0,0 +1,124 @@
+<template>
+
+
+<div class="SignInAlignment">
+  <form>
+        <h3 >#Q : {{this.QuestionNo + 1}} </h3>
+        <input type="text" class="inputStyle" placeholder="Question">
+        <textarea class="inputStyle pa2 " name="message" rows="6" style="width:100%;" placeholder="Your Answer Here"></textarea>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        QuestionNo :String
+
+    }
+}
+</script>
+
+
+<style scoped>
+textarea{
+  max-width:100%;
+  min-width:100%;
+}
+
+h3{
+  color:white;
+  background-color:white;
+  border-radius:12px;
+  border: 2px solid #008CBA;
+  text-align:left;
+  padding:5px;
+  width:20% ;
+  background-color: #01142F;
+	transition-duration: 0.4s;
+  cursor:pointer;
+}
+h4{
+    min-height:40px !important;
+    cursor:pointer;
+}
+.SignInAlignment {
+    display: flex;
+    justify-content : center;
+    font-family: "Comic Sans MS", cursive, sans-serif ;
+    width: 100%;
+    height: 100%;
+    opacity: 1 !important;
+}
+
+form{
+    width:80% !important;
+}
+
+.inputStyle {
+    color: white !important;
+    border-radius: 12px;
+    background-color: #01142F;
+	  transition-duration: 0.4s;
+    border: 2px solid #008CBA;
+    width: 100%;
+    font-size: 1.5rem;
+    padding:8px;
+    margin:2px;
+}
+.row{
+  display:flex;
+  width:100%;
+  height: auto;
+}
+
+.buttonStyle:hover {
+  background-image: linear-gradient(89deg,#3CBA92,#0BA360);
+  color: black ;
+}
+.otherColumns{
+  display: flex;
+}
+#columnLeft{
+  width:20% !important;
+  height: auto;
+}
+
+#columnRight{
+  width:80%  !important;
+  height: auto;
+}
+
+.buttonStyle{
+  border-radius: 12px;
+  background-color: powderblue;
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  align-content: center;
+  font-size: 1rem;
+  margin: 4px 2px;
+  transition-duration: 0.2s;
+  cursor: pointer;
+  width: 50%;
+  border: 2px solid #008CBA;
+}
+
+
+.buttonStyle:active{
+  background-color: royalblue;
+  color: red;
+  box-shadow: 0 5px #666;
+  transform: translateY(2px);
+}
+
+
+
+.inputStyle:hover{
+  border-color:purple;
+  background-image: linear-gradient(89deg,#3CBA92,#0BA360);
+}
+</style>
