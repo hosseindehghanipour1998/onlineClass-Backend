590afb65557112b84680dca030c54d703c19ca23
Hossein <hossein.dehghanipour1998@gmail.com>
2020-08-03 03:39:11 +0430

    Added BackEnd Codes

---
+++ BackEnd/backEnd/systemCenter/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SystemcenterConfig(AppConfig):
+    name = 'systemCenter'
---
+++ BackEnd/backEnd/systemCenter/serializers.py
@@ -0,0 +1,116 @@
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.utils.crypto import get_random_string
+
+from .models import UserProfile, Room, Exam, Question,ExamAnswer, Answer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'password', 'username','email']
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'user', 'token']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data.pop('user'))
+        profile = UserProfile(**validated_data, user=user)
+        profile.save()
+
+        return profile
+
+
+class AuthSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+class RoomCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ['id', 'name', 'link', 'admin' ,'description']
+        '''
+        extra_kwargs = {
+            "link": {"read_only": True}
+        }
+        '''
+    def create(self, validated_data):
+        #link = get_random_string(length=32)
+        admin = validated_data.pop('admin')[0]
+
+        room = Room(**validated_data)
+        room.save()
+        room.admin.add(admin)
+
+        return room
+
+
+class RoomRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ['id', 'name', 'link', 'admin', 'participate','description']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class ExamRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+
+class ExamSerializer(serializers.ModelSerializer):
+    questions = serializers.ListField(write_only=True)
+
+    class Meta:
+        model = Exam
+        #fields = ['id', 'title', 'start_time', 'end_time', 'room', 'questions']
+        fields = ['id', 'title','room', 'questions']
+
+    def create(self, validated_data):
+        questions = validated_data.pop('questions')
+        exam = Exam.objects.create(**validated_data)
+
+        for question in questions:
+            Question.objects.create(exam=exam, **question)
+
+        return exam
+    
+class RetrieveExamQuestions(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    answers = serializers.ListField(write_only=True)
+
+    class Meta:
+        model = ExamAnswer
+        fields = ['id', 'user_profile', 'exam', 'answers']
+
+    def create(self, validated_data):
+        answers = validated_data.pop('answers')
+        exam_answer = ExamAnswer.objects.create(**validated_data)
+
+        for answer in answers:
+            question = Question.objects.get(id=answer.pop('question'))
+            Answer.objects.create(exam_answer=exam_answer, question=question, **answer)
+
+        return exam_answer
+        
+
